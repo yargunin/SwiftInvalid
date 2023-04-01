f84d26722b8184ae6e4c59c268d502df75df3863
yargunin <vip.yargunin@mail.ru>
2023-04-01 13:08:06 +0400

    все переходы + галочка(надо тестить на телефоне)

--- SwiftInvalid/TabBar/TabBarController.swift
+++ SwiftInvalid/TabBar/TabBarController.swift
@@ -12,6 +12,10 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
+
+        
         self.navigationItem.hidesBackButton = true
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Назад", style: .plain, target: nil, action: nil)
 
