fc7e02a6fa1182ba1364cd19d911261e88206b77
yargunin <vip.yargunin@mail.ru>
2023-04-16 17:39:36 +0400

    поставил изображение

--- SwiftInvalid/TabBar/Themes/TheBasics/TuplesViewController.swift
+++ SwiftInvalid/TabBar/Themes/TheBasics/TuplesViewController.swift
@@ -19,71 +19,22 @@ class TuplesViewController: UIViewController {
         // Создание UIScrollView
         let scrollView = UIScrollView(frame: view.bounds)
         scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1100)
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 2000)
         scrollView.isScrollEnabled = true
         
        
         
        
-            let imageAttachment = NSTextAttachment()
-            let originalImage = UIImage(named: "Снимок экрана 2023-04-16 в 14.41.21.png")
-        let resizedImage = originalImage?.resize(toWidth: 380, height: 100) // Replace 300 with the desired width
-            imageAttachment.image = resizedImage
-            let imageString = NSAttributedString(attachment: imageAttachment)
+        let imageAttachment = NSTextAttachment()
+        let originalImage = UIImage(named: "image.png")
+        let resizedImage = originalImage?.resize(toWidth: 360, height: 1800) // Replace 300 with the desired width
+        imageAttachment.image = resizedImage
+        let imageString = NSAttributedString(attachment: imageAttachment)
 
-            let textString = "Кортежи группируют несколько значений в одно составное значение. Значения внутри кортежа могут быть любого типа, то есть, нет необходимости, чтобы они были одного и того же типа.\n\nВ данном примере (404, \"Not Found\") это кортеж, который описывает код HTTP статуса. Код HTTP статуса — особое значение, возвращаемое веб-сервером каждый раз, когда вы запрашиваете веб-страницу. Код статуса 404 Not Found возвращается, когда вы запрашиваете страницу, которая не существует.\n\n"
+        LableInfo.attributedText = imageString
 
-            let attributedString = NSMutableAttributedString(string: textString, attributes: [
-                .font: UIFont.systemFont(ofSize: 17),
-                .foregroundColor: UIColor.black
-            ])
-            attributedString.append(imageString)
-
-            LableInfo.attributedText = attributedString
-        
-
-        // Продолжение текста здесь
-        if let attributedText = LableInfo.attributedText {
-            let continuationString = "Чтобы передать код статуса, кортеж (404, \"Not Found\") группирует вместе два отдельных значения Int и String: число и понятное человеку описание. Это может быть описано как \"кортеж типа (Int, String)\".\n\nВы можете создать кортеж с любой расстановкой типов, и они могут содержать сколько угодно нужных вам типов. Ничто вам не мешает иметь кортеж типа (Int, Int, Int), или типа (String, Bool), или же с любой другой расстановкой типов по вашему желанию.\n\nВы можете разложить содержимое кортежа на отдельные константы и переменные, к которым можно получить доступ привычным способом:"
-            let continuationAttributedString = NSAttributedString(string: continuationString)
-            let newAttributedString = NSMutableAttributedString(attributedString: attributedText)
-            newAttributedString.append(continuationAttributedString)
-            LableInfo.attributedText = newAttributedString
-        }
-        
-        // Создание нового изображения
-        if let newImage = UIImage(named: "Снимок экрана 2023-04-16 в 15.20.03.png") {
-            // Изменение размера изображения
-            let resizedImage = newImage.resize(toWidth: 380, height: 100)
-
-            // Создание нового вложения изображения
-            let newImageAttachment = NSTextAttachment()
-            newImageAttachment.image = resizedImage
-
-            // Создание нового NSAttributedString из вложения изображения
-            let newImageAttributedString = NSAttributedString(attachment: newImageAttachment)
-
-            // Добавление нового NSAttributedString в конец существующего атрибутированного текста
-            if let attributedText = LableInfo.attributedText {
-                let newAttributedString = NSMutableAttributedString(attributedString: attributedText)
-                newAttributedString.append(NSAttributedString(string: "\n"))
-                newAttributedString.append(newImageAttributedString)
-                LableInfo.attributedText = newAttributedString
-            }
-        }
       
 
-        
-
-
-
-
-        
-
-        
-        LableInfo.lineBreakMode = .byWordWrapping
-        LableInfo.numberOfLines = 0
-        LableInfo.sizeToFit()
         
         // Добавление UILabel в UIScrollView
         scrollView.addSubview(LableInfo)
