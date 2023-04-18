962214d0f9ae3f6b2e4ce7abe1c53e91e07de8e7
yargunin <vip.yargunin@mail.ru>
2023-04-18 21:09:32 +0400

    1

--- SwiftInvalid/TabBar/Themes/TheBasics/AnnotationOfTypesViewController.swift
+++ SwiftInvalid/TabBar/Themes/TheBasics/AnnotationOfTypesViewController.swift
@@ -24,33 +24,13 @@ class AnnotationOfTypesViewController: UIViewController {
         
        
         
-        LableInfo.text = """
-        Вы можете добавить обозначение типа, когда объявляете константу или переменную, чтобы иметь четкое представление о типах значений, которые могут хранить константы или переменные. Написать обозначение типа, можно поместив двоеточие после имени константы или переменной, затем пробел, за которым следует название используемого типа.
+        let imageAttachment = NSTextAttachment()
+        let originalImage = UIImage(named: "Аннотация типов")
+        let resizedImage = originalImage?.resize(toWidth: 365, height: 1000)
+        imageAttachment.image = resizedImage
+        let imageString = NSAttributedString(attachment: imageAttachment)
 
-        Этот пример добавляет обозначение типа для переменной с именем welcomeMessage, чтобы обозначить, что переменная может хранить String:
-
-        var welcomeMessage: String
-        Двоеточие в объявлении значит "...типа...", поэтому код выше может быть прочитан как:
-
-        «Объяви переменную с именем welcomeMessage, тип которой будет String»
-
-        Фраза «тип которой будет String» означает «может хранить любое String значение». Представьте, что словосочетание «тип которой будет такой-то» означает - значение, которое будет храниться.
-
-        Теперь переменной welcomeMessage можно присвоить любое текстовое значение, без каких либо ошибок:
-
-        welcomeMessage = "Hello"
-        Вы можете создать несколько переменных одного типа в одной строке, разделенной запятыми, с одной аннотацией типа после последнего имени переменной:
-
-        var red, green, blue: Double
-        Заметка
-        Редко когда вам понадобится обозначать тип на практике. Когда вы даете начальное значение константе или переменной на момент объявления, Swift всегда может вывести тип, который будет использовать в константе или переменной. Это описано в Строгая типизация и Вывод типов. В примере welcomeMessage выше, не было присвоения начального значения, так что тип переменной welcomeMessage указывается с помощью обозначения типа вместо того, чтобы вывести из начального значения.
-
-
-        """
-        LableInfo.lineBreakMode = .byWordWrapping
-        LableInfo.font = UIFont.systemFont(ofSize: 17)
-        LableInfo.numberOfLines = 0
-        LableInfo.sizeToFit()
+        LableInfo.attributedText = imageString
         
         // Добавление UILabel в UIScrollView
         scrollView.addSubview(LableInfo)
@@ -60,7 +40,7 @@ class AnnotationOfTypesViewController: UIViewController {
         
         // Создание кнопки
         let button = UIButton(type: .system)
-        button.setTitle("Печать констант и переменных", for: .normal)
+        button.setTitle("Название констант и переменных", for: .normal)
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17) // Установка жирного шрифта для заголовка кнопки
         button.frame = CGRect(x: scrollView.contentSize.width - 310, y: scrollView.contentSize.height, width: 300, height: -100)
--- SwiftInvalid/TabBar/Themes/TheBasics/logicalTypesViewController.swift
+++ SwiftInvalid/TabBar/Themes/TheBasics/logicalTypesViewController.swift
@@ -19,48 +19,18 @@ class logicalTypesViewController: UIViewController {
         // Создание UIScrollView
         let scrollView = UIScrollView(frame: view.bounds)
         scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        scrollView.contentSize = CGSize(width: view.bounds.width, height: 2200)
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1300)
         scrollView.isScrollEnabled = true
         
        
         
-        LableInfo.text = """
-        В Swift есть простой логический тип Bool. Этот тип называют логическим, потому что он может быть только true или false. Swift предусматривает две логические константы, true и false соответственно:
+        let imageAttachment = NSTextAttachment()
+        let originalImage = UIImage(named: "Логические типы")
+        let resizedImage = originalImage?.resize(toWidth: 365, height: 1200)
+        imageAttachment.image = resizedImage
+        let imageString = NSAttributedString(attachment: imageAttachment)
 
-        let orangesAreOrange = true
-        let turnipsAreDelicious = false
-        Типы для orangesAreOrange и turnipsAreDelicious были выведены как Bool, исходя из того факта, что мы им присвоили логические литералы. Так же как с Int и Double в предыдущих главах, вам не нужно указывать константы или переменные как Bool, если при создании вы присвоили им значения true или false. Вывод типов помогает сделать код Swift кратким и читабельным тогда, когда вы создаете константы или переменные со значениями которые точно известны.
-
-        Логические значения очень полезны когда вы работаете с условными операторами, такими как оператор if:
-
-        if turnipsAreDelicious {
-         print("Mmm, tasty turnips!")
-        } else {
-         print("Eww, turnips are horrible.")
-        }
-        // Выведет "Eww, turnips are horrible."
-        Условные операторы, такие как оператор if детально рассматриваются в главе Управление потоком.
-
-        Строгая типизация Swift препятствует замене значения Bool на не логическое значение. Следующий пример выведет ошибку компиляции:
-
-        let i = 1
-        if i {
-         // этот пример не скомпилируется, и выдаст ошибку компиляции
-        }
-        Тем не менее, альтернативный пример ниже правильный:
-
-        let i = 1
-        if i == 1 {
-         // этот пример выполнится успешно
-        }
-        Результат сравнения i == 1 имеет тип Bool, и поэтому этот второй пример совершает проверку типов. Такие сравнения как i == 1 обсуждаются в главе Базовые операторы.
-
-        Как в других примерах строгой типизации в Swift, этот подход предотвращает случайные ошибки и гарантирует, что замысел определенной части кода понятен.
-        """
-        LableInfo.lineBreakMode = .byWordWrapping
-        LableInfo.font = UIFont.systemFont(ofSize: 17)
-        LableInfo.numberOfLines = 0
-        LableInfo.sizeToFit()
+        LableInfo.attributedText = imageString
         
         // Добавление UILabel в UIScrollView
         scrollView.addSubview(LableInfo)
